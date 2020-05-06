0a93343614cbc65bf50a3f80ea95eff233529327
Ayoob7 <mohamedayoob01@gmail.com>
2020-05-07 00:12:37 +0530

    refined

--- Dashboard.py
+++ Dashboard.py
@@ -14,7 +14,7 @@ def train_and_recognise_model():
     os.system("py 2_facenet.py")
 
 def attend():
-    #os.startfile(os.getcwd() + "/firebase/attendance_files/attendance" + str(datetime.now().date()) + '.xls')
+    #os.startfile(os.getcwd() + "\\reports\\attendance" + str(datetime.now().date()) + '.xls')
     pass
 
 def deleteStudents():
@@ -42,29 +42,29 @@ Button(root, text="Add Person", font=("sans serif", 20), bg="#587795", fg='white
                                                                                                             pady=5)
 
 # creating second button
-Button(root, text="Train and Recognise Model", font=("sans serif", 20), bg="#587795", fg='white', command=train_and_recognise_model).grid(row=3,
-                                                                                                             column=1,
+Button(root, text="Train and Recognise Model", font=("sans serif", 20), bg="#587795", fg='white', command=train_and_recognise_model).grid(row=4,
+                                                                                                             column=0,
                                                                                                              sticky=N + E + W + S,
                                                                                                              padx=5,
                                                                                                              pady=5)
 
 
 # creating attendance button
-Button(root, text="Attendance Sheet", font=('calibri', 20), bg="#587795", fg="white", command=attend).grid(row=4,
-                                                                                                           column=1,
+Button(root, text="Attendance Sheet", font=('calibri', 20), bg="#587795", fg="white", command=attend).grid(row=5,
+                                                                                                           column=0,
                                                                                                            sticky=N + E + W + S,
                                                                                                            padx=5,
                                                                                                            pady=5)
 
 
-Button(root, text="Delete Person", font=('calibri', 20), bg="#587795", fg="white", command=deleteStudents).grid(row=5,
-                                                                                                                column=1,
+Button(root, text="Delete Person", font=('calibri', 20), bg="#587795", fg="white", command=deleteStudents).grid(row=6,
+                                                                                                                column=0,
                                                                                                                 sticky=N + E + W + S,
                                                                                                                 padx=5,
                                                                                                                 pady=5)
 
 # exit
-Button(root, text="Exit", font=('calibri', 20), bg="#3D003D", fg="white", command=exitProgram).grid(row=9, columnspan=2,
+Button(root, text="Exit", font=('calibri', 20), bg="#3D003D", fg="white", command=exitProgram).grid(row=7, columnspan=2,
                                                                                                     padx=5, pady=5)
 
 root.mainloop()
